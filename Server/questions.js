28df03340dd0b8001b9312d90797ee5d103a0e99
Magnus H <me@magnushodne.com>
2022-03-01 14:36:23 +0100

    Function for verifying correct answers

--- Server/questions.js
+++ Server/questions.js
@@ -30,3 +30,10 @@ export const Questions = [
 export function randomQuestion() {
   return Questions[Math.trunc(Math.random() * Questions.length)];
 }
+
+/// Checks whether the answer passed in is the correct one for a given question id.
+/// The format of 'answer' should be 'answer_x', where x is a,b,c,d etc...
+export function isCorrectAnswer(id, answer) {
+  const question = Questions.find((q) => q.id === id);
+  return question.correct_answers[answer + "_correct"] === "true";
+}
