77e38bd84781d96fd8fb880fae2407ee44810173
Magnus H <me@magnushodne.com>
2022-03-01 12:39:14 +0100

    Added server functionality for serving React app

--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,17 @@
 import express from "express";
+import * as path from "path";
 
 //app is the "program" that will run on our server
 const app = express();
+
+app.use(express.static("..Client/dist"));
+app.use((req, res, next) => {
+  if (req.method === "GET" && !req.path.startsWith("/api/")) {
+    return res.sendFile(path.resolve("..Client/dist/index.html"));
+  } else {
+    next();
+  }
+});
 
 //actually starts our server
 const server = app.listen(process.env.PORT || 3000, () => {
