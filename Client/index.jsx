f56f61fbd537368c1d7c6d7989efba143c66ceea
Magnus H <me@magnushodne.com>
2022-03-01 15:23:44 +0100

    Application can fetch questions from server

--- Client/index.jsx
+++ Client/index.jsx
@@ -1,8 +1,42 @@
+import React, { useState } from "react";
 import * as ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-function Question() {
-  return <h1>This should show the question</h1>;
+function QuestionComponent() {
+  const [question, setQuestion] = useState();
+
+  async function handleLoadQuestion() {
+    const res = await fetch("/api/question");
+    setQuestion(await res.json());
+  }
+
+  if (!question) {
+    return (
+      <div>
+        <button onClick={handleLoadQuestion}>Load a new question</button>
+      </div>
+    );
+  }
+
+  return <QuestionDisplay question={question} />;
+}
+
+///This function actually renders the question with all the answers etc.
+function QuestionDisplay({ question }) {
+  return (
+    <div>
+      <h2>{question.question}</h2>
+      {Object.keys(question.answers)
+        .filter((a) => question.answers[a])
+        .map((a) => (
+          <div key={a}>
+            <button onClick={() => console.log("Should handle answer")}>
+              {question.answers[a]}
+            </button>
+          </div>
+        ))}
+    </div>
+  );
 }
 
 function Answer() {
@@ -14,9 +48,6 @@ function PageLinks() {
     <div>
       <div>
         <Link to={"/question"}>Random question</Link>
-      </div>
-      <div>
-        <Link to={"answer"}>Answer template</Link>
       </div>
     </div>
   );
@@ -35,7 +66,7 @@ function Application() {
     <BrowserRouter>
       <Routes>
         <Route path={"/"} element={<FrontPage />} />
-        <Route path={"/question"} element={<Question />} />
+        <Route path={"/question"} element={<QuestionComponent />} />
         <Route path={"/answer"} element={<Answer />} />
       </Routes>
     </BrowserRouter>
