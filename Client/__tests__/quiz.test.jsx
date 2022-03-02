d72b4f2ffc664da2d64a673df20de5ffd839f992
Magnus H <me@magnushodne.com>
2022-03-02 15:13:23 +0100

    Re-implemented a snapshot function

--- Client/__tests__/quiz.test.jsx
+++ Client/__tests__/quiz.test.jsx
@@ -1,11 +1,62 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { FrontPage } from "../quizApplication";
+import ReactDOM, { unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+import { QuizComponent } from "../quizComponent";
+import { fetchJSON, submitJSON } from "../http";
 
-describe("quiz application", () => {
-  it("shows a question", () => {
-    const element = document.createElement("div");
-    ReactDOM.render(<FrontPage />, element);
-    expect(element.innerHTML).toMatchSnapshot();
+const question = {
+  id: 1,
+  question: "Are you happy?",
+  category: "personal",
+  answers: {
+    answer_a: "Yes",
+    answer_b: "No",
+    answer_c: "Maybe",
+  },
+};
+
+class QuizApi {
+  async getQuestion() {
+    return question;
+  }
+
+  async checkAnswer(id, answer) {
+    return "true";
+  }
+}
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+describe("A quiz component", () => {
+  it("looks the same", async () => {
+    await act(async () => {
+      await ReactDOM.render(
+        <QuizComponent quizApi={new QuizApi()} />,
+        container
+      );
+    });
+    expect(container.innerHTML).toMatchSnapshot();
+  });
+
+  it("shows a question", async () => {
+    // Use the asynchronous version of act to apply resolved promises
+    await act(async () => {
+      await ReactDOM.render(
+        <QuizComponent quizApi={new QuizApi()} />,
+        container
+      );
+    });
+    expect(container.textContent).toContain(question.question);
   });
 });
