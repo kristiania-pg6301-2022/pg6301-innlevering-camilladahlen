17827a94f2e6f0329921fae65533862ad3e34e74
Magnus H <me@magnushodne.com>
2022-03-02 14:03:15 +0100

    Refactored submitJSON to allow for multiple methods

--- Client/http.js
+++ Client/http.js
@@ -16,13 +16,13 @@ export async function fetchJSON(url) {
 /// Function for posting JSON data to a url. Throws an error if response code isn't 200.
 /// Also returns the response message, which you can then call .json() or .text() on,
 /// depending upon what the server spits back
-export async function postJSON(url, json) {
+export async function submitJSON(url, method, payload) {
   const res = await fetch(url, {
-    method: "post",
+    method: method,
     headers: {
       "content-type": "application/json",
     },
-    body: JSON.stringify(json),
+    body: JSON.stringify(payload),
   });
   if (!res.ok) {
     throw new HttpError(res.status, res.statusText);
