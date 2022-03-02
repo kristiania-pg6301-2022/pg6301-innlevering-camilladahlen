3c895e367306230396d2ca71ad469749c704e9b8
Magnus H <me@magnushodne.com>
2022-03-02 13:40:33 +0100

    Removed reload function from loader

--- Client/useLoader.jsx
+++ Client/useLoader.jsx
@@ -1,23 +1,21 @@
 import { useEffect, useState } from "react";
 
 /// Custom hook that can be used in React apps
-export function useLoader(loadFn) {
-  const [loading, setLoading] = useState(true);
+export function useLoader(loader) {
   const [data, setData] = useState();
   const [error, setError] = useState();
+  const [loading, setLoading] = useState(true);
 
-  async function reload() {
+  useEffect(async () => {
     setError(undefined);
     setLoading(true);
     try {
-      setData(await loadFn());
+      setData(await loader());
     } catch (error) {
       setError(error);
     } finally {
       setLoading(false);
     }
-  }
-
-  useEffect(reload, []);
-  return { reload, loading, data, error };
+  }, []);
+  return { loading, data, error };
 }
