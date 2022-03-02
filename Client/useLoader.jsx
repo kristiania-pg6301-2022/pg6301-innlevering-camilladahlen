29cf9f1956c693f1d06567cab78ef3d794ff795c
Magnus H <me@magnushodne.com>
2022-03-02 14:12:29 +0100

    Documented useLoader.jsx

--- Client/useLoader.jsx
+++ Client/useLoader.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
 
-/// Custom hook that can be used in React apps
+/// Hook for loading data. It will call a loader function (for example a fetch call) for you
+/// and set the data property when the data is loaded. If the data wasn't loaded successfully,
+/// the error property is set. You can also feed it some dependencies. This ensures that the
+/// data is reloaded when the dependencies change
 export function useLoader(loader, deps) {
   const [data, setData] = useState();
   const [error, setError] = useState();
